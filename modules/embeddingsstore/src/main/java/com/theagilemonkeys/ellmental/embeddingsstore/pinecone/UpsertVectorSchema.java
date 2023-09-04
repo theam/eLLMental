1df9e73b106db328bb0f488f1ff74399206e995b
juanjoman <j2rgez@gmail.com>
2023-09-04 11:40:01 +0100

    use namespace from pinecone store constructor

--- modules/embeddingsstore/src/main/java/com/theagilemonkeys/ellmental/embeddingsstore/pinecone/UpsertVectorSchema.java
+++ modules/embeddingsstore/src/main/java/com/theagilemonkeys/ellmental/embeddingsstore/pinecone/UpsertVectorSchema.java
@@ -2,17 +2,24 @@ package com.theagilemonkeys.ellmental.embeddingsstore.pinecone;
 
 
 import com.theagilemonkeys.ellmental.core.schema.Embedding;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
 import java.util.List;
 import java.util.ArrayList;
 
-
+@Getter
+@ToString
+@EqualsAndHashCode
 public class UpsertVectorSchema {
-    List<Vector> vectors;
+    private final List<Vector> vectors;
+    private final String namespace;
 
-    UpsertVectorSchema(Embedding embedding) {
-        vectors = new ArrayList<>();
-        vectors.add(new Vector(embedding.id(), embedding.vector(), embedding.metadata()));
+    public UpsertVectorSchema(Embedding embedding, String namespace) {
+        this.vectors = new ArrayList<>();
+        this.vectors.add(new Vector(embedding.id(), embedding.vector(), embedding.metadata()));
+        this.namespace = namespace;
     }
 
 }
