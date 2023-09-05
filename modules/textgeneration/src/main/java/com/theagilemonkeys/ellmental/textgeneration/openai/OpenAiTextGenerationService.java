382f572ca1957228444eaa2d21adaa2fb4716b3a
juanjoman <j2rgez@gmail.com>
2023-09-05 12:13:46 +0100

    add logback

--- modules/textgeneration/src/main/java/com/theagilemonkeys/ellmental/textgeneration/openai/OpenAiTextGenerationService.java
+++ modules/textgeneration/src/main/java/com/theagilemonkeys/ellmental/textgeneration/openai/OpenAiTextGenerationService.java
@@ -5,6 +5,8 @@ import com.theokanning.openai.completion.chat.ChatCompletionChoice;
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
 import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.service.OpenAiService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.util.List;
@@ -12,7 +14,7 @@ import java.util.List;
 import static com.theagilemonkeys.ellmental.textgeneration.openai.errors.Constants.NO_CONTENT_FOUND_OPEN_AI;
 
 public class OpenAiTextGenerationService extends TextGenerationService<List<ChatMessage>, OpenAiModels> {
-
+    private static final Logger log = LoggerFactory.getLogger(OpenAiTextGenerationService.class);
     private final OpenAiService client;
 
     public OpenAiTextGenerationService(String openAiKey) {
@@ -26,6 +28,7 @@ public class OpenAiTextGenerationService extends TextGenerationService<List<Chat
 
     @Override
     public String generate(List<ChatMessage> chatMessages, Double temperature, int maxTokens, OpenAiModels model) {
+        log.debug("Generating chat response for chat messages {}", chatMessages);
         ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest.builder()
                 .messages(chatMessages)
                 .temperature(temperature)
@@ -35,6 +38,8 @@ public class OpenAiTextGenerationService extends TextGenerationService<List<Chat
 
         ChatCompletionChoice chatCompletionChoice = client.createChatCompletion(chatCompletionRequest).getChoices().get(0);
         String chatCompletionContent = chatCompletionChoice.getMessage().getContent();
+
+        log.debug("Chat completion response is {}", chatCompletionContent);
 
         return !chatCompletionContent.isEmpty() ?
                 chatCompletionContent :
